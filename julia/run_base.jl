b646bb005bd2dfa4b9f9d02c40f658bb5af23029
Sophie Zhou <sophiezy@umich.edu>
2025-11-17 14:28:04 -0500

    added dengue caseload from Indonesia

--- julia/run_base.jl
+++ julia/run_base.jl
@@ -6,3 +6,17 @@ Pkg.activate(@__DIR__)
 using DifferentialEquations
 using Plots
 using .BaseDengueModel
+
+
+# --- Extract solution ---
+t   = sol.t
+S_H = getindex.(sol.u, 1)
+E_H = getindex.(sol.u, 2)
+I_H = getindex.(sol.u, 3)
+R_H = getindex.(sol.u, 4)
+S_M = getindex.(sol.u, 5)
+I_M = getindex.(sol.u, 6)
+
+# --- Plot humans ---
+
+# --- Plot mosquitoes ---
